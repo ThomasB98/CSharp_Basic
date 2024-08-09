a35550c7ba9064155c93dce32db0393d248963e8
ThomasB98 <78976644+ThomasB98@users.noreply.github.com>
2024-08-09 16:17:21 +0530

    Add project files.

---
+++ EmployeeWages/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
